3b3f77b749645ff99dc9f2ec90bcba3a1c8b5115
chkim0825 <chkim@nextree.co.kr>
2019-06-07 15:58:57 +0900

    1 file modified

--- machine/Drink.js
+++ machine/Drink.js
@@ -17,16 +17,14 @@ class Drink extends React.Component {
       <Segment inverted style={{ width: 100 }}>
         <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
         <Card.Content>
-          <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
-          <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
+          <Card.Header style={{ textAlign: 'center' }}></Card.Header>
+          <Card.Meta style={{ textAlign: 'center' }}>₩ </Card.Meta>
         </Card.Content>
         <Card.Content extra >
-          <Card.Header style={{ textAlign: 'center' }}>
+          <Card.Header style={{ textAlign: 'center' }}> d
             <Button size='mini' as='div' labelPosition='right'>
-              <Button size='mini' color='red' onClick={()=>this.onClickGetButton()}>GET</Button>
-              <Label as='a' basic color='red' pointing='left'>
-                {item.quantity}
-              </Label>
+              <Button size='mini' color='red' >GET</Button>
+              <Label as='a' basic color='red' pointing='left'></Label>
             </Button>
           </Card.Header>
         </Card.Content>
