26761ba03ecd6d914bc216df4fd63fd24117f883
chkim0825 <chkim@nextree.co.kr>
2019-06-07 16:01:46 +0900

    1 file modified

--- machine/Drink.js
+++ machine/Drink.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { Segment, Card, Icon, Image, Label, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 class Drink extends React.Component {
-
-  static defaultProps = {
-    item: {
-      name: 'empty',
-      price: 0,
-      quantity: 0
-    }
-  }
-
   render() {
     const { item } = this.props;
     return (
@@ -21,7 +12,7 @@ class Drink extends React.Component {
           <Card.Meta style={{ textAlign: 'center' }}>₩ </Card.Meta>
         </Card.Content>
         <Card.Content extra >
-          <Card.Header style={{ textAlign: 'center' }}> d
+          <Card.Header style={{ textAlign: 'center' }}> 
             <Button size='mini' as='div' labelPosition='right'>
               <Button size='mini' color='red' >GET</Button>
               <Label as='a' basic color='red' pointing='left'></Label>
@@ -34,45 +25,23 @@ class Drink extends React.Component {
 }
 
 
-// const Drink = ({ item }) => {
-
+// const Drink = () => {
 //   return (
 //       <Segment inverted style={{ width: 100 }}>
 //         <Image src={'https://i.dlpng.com/static/png/1238556-chips-chips-and-soda-png-800_800_preview.png'} size='mini' centered />
 //         <Card.Content>
-//           <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
-//           <Card.Meta style={{ textAlign: 'center' }}>₩ {item.price}</Card.Meta>
+//           <Card.Header style={{ textAlign: 'center' }}></Card.Header>
+//           <Card.Meta style={{ textAlign: 'center' }}>₩ </Card.Meta>
 //         </Card.Content>
 //         <Card.Content extra >
 //           <Card.Header style={{ textAlign: 'center' }}>
 //             <Button size='mini' as='div' labelPosition='right'>
-//               <Button size='mini' color='red' onClick={()=>this.onClickGetButton()}>GET</Button>
-//               <Label as='a' basic color='red' pointing='left'>
-//                 {item.quantity}
-//               </Label>
+//               <Button size='mini' color='red' >GET</Button>
+//               <Label as='a' basic color='red' pointing='left'></Label>
 //             </Button>
 //           </Card.Header>
 //         </Card.Content>
 //       </Segment>
 //   )
 // }
-
-// Drink.defaultProps = {
-//   item: {
-//     name: 'empty',
-//     price: 0,
-//     quantity: 0
-//   }
-// }
-
-const itemShape = {
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  quantity: PropTypes.number.isRequired
-}
-
-Drink.propTypes = {
-  item: PropTypes.shape(itemShape)
-}
-
 export default Drink;
